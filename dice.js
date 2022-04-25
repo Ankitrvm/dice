514c63dec48a02cffb80befe08d9e8ea00267c60
chikuhere <ankitkumar2082001@gmail.com>
2022-04-26 01:22:41 +0530

    dice game initial code

---
+++ dice.js
@@ -0,0 +1,41 @@
+
+document.querySelector("button").addEventListener("click", handleClicked);
+
+function handleClicked(){
+
+    var randomNumber1 = Math.floor(Math.random() * 6 )+ 1;
+
+    var randomImage = "dice" + randomNumber1 + ".png";
+    
+    var randomImsgeSource = "image/" + randomImage;
+    
+    var image1 = document.querySelectorAll("img")[0];
+     
+    image1.setAttribute("src", randomImsgeSource);
+    
+    
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    
+    var randomImage2 = "dice" + randomNumber2 + ".png";
+    
+    var randomImageSource2 = "image/" + randomImage2;
+    
+    var image2 = document.querySelectorAll("img")[1];
+    
+    image2.setAttribute("src", randomImageSource2);
+
+
+
+
+
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "player 1 wins!"
+    }
+    else if(randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "player 2 wins!"
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw!!!!!"
+    }
+}
